9b6c6054e8c4b48a8cb3ab9c000cd14894d4ecb9
QuintonJason <quinton.jason@gmail.com>
2019-05-08 19:05:31 -0500

    added react-axe for accessibility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axe from 'react-axe';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+  ReactDOM.render(<App />, document.getElementById('root'));
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
